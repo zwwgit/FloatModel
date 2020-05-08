ac4f840952ed64e7004711806d9a35af7aafd140
zwwgit <1565280103@qq.com>
2020-05-08 18:21:44 +0800

    hover/close

--- test.js
+++ test.js
@@ -7,40 +7,48 @@
     }
     var FloatModel = function (option) {
         this.option = option ? defaultOption.concat(option) : defaultOption
-        this.el = typeof this.option.el === "string" ? doc.querySelector(this.option.el) : this.option.el
+        this.el = doc.querySelector(this.option.el)
         this.el.style.position = 'fixed'
         this.el.style.left = '0px'
         this.el.style.top = '0px'
-        this.el.style.animation = 'float-row-animation linear ' + win.innerWidth / this.option.speed + 's infinite,float-col-animation linear ' + win.innerHeight / this.option.speed + 's infinite'
-    }
+        this.el.style.animation = 'float-row-animation linear ' + doc.documentElement.clientWidth / this.option.speed + 's infinite,float-col-animation linear ' + doc.documentElement.clientHeight / this.option.speed + 's infinite'
 
+    }
     FloatModel.prototype.start = function () {
         const rowkeyframes = ` @keyframes float-row-animation{
             0%{
                 left: 0px;
             }
             50%{
-                left:${win.innerWidth - this.el.offsetWidth}px;
+                left:${doc.documentElement.clientWidth  - this.el.offsetWidth}px;
             }
             100%{
                 left: 0px;
             }
-        }`
-        const colkeyframes = ` @keyframes float-col-animation{
+        }
+        @keyframes float-col-animation{
             0%{
                 top: 0px;
             }
             50%{
-                top:${win.innerHeight - this.el.offsetHeight}px;
+                top:${doc.documentElement.clientHeight - this.el.offsetHeight}px;
             }
             100%{
                 top: 0px;
             }
-        }`
+        }
+        ${this.option.el}:hover{
+            animation-play-state:paused !important;
+        }
+        `
         const style = doc.createElement('style')
         style.type = "text/css"
-        style.innerHTML = rowkeyframes + colkeyframes
+        style.innerHTML = rowkeyframes
         doc.getElementsByTagName('head')[0].appendChild(style)
+
+    }
+    FloatModel.prototype.close=function(){
+        this.el.style.display='none'
     }
     if (typeof module !== 'undefined' && module.exports) module.exports = FloatModel;
     if (typeof define === 'function') define(function () { return FloatModel; });
